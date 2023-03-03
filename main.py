f21ab09e3a49f8c4cb8db51f7c03618de288d276
Kerem <88890068+linuxkerem@users.noreply.github.com>
2023-03-03 16:36:50 +0300

    Tum verileri gosterme ve veri tabani temizleme metodlari eklendi

--- main.py
+++ main.py
@@ -1,31 +1,79 @@
 from requiredLibraries import *
-
+from mainWindow import  Ui_Dialog
 class Database():
     def __init__(self):
         self.conn = sqlite3.connect('logs.db')
         self.c = self.conn.cursor()
         self.c.execute("CREATE TABLE IF NOT EXISTS main (id INTEGER PRIMARY KEY, date TEXT, type INTEGER, amount REAL)")
         self.conn.commit()
-        #self.conn.close()
-
-class Main():
+    def insert(self, date, type, amount):
+        self.c.execute("INSERT INTO main (date, type, amount) VALUES (?, ?, ?)", (date, type, amount))
+        self.conn.commit()
+    def clear(self):
+        self.c.execute("DELETE FROM main;")
+        self.conn.commit()
+        print("Veritabanı temizlendi.")
+class dbWindow(QMainWindow):
     def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Database Window")
+        self.resize(450, 250)
+        self.view = QTableWidget()
+        self.view.setColumnCount(4)
+        self.view.setHorizontalHeaderLabels(["ID", "Date", "Type", "Amount"])
+        self.view.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.view.setSelectionBehavior(QTableWidget.SelectRows)
+        self.view.setSelectionMode(QTableWidget.SingleSelection)
+        self.view.setShowGrid(False)
+        self.view.verticalHeader().setVisible(False)
+        self.view.horizontalHeader().setStretchLastSection(True)
+        self.view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.setCentralWidget(self.view)
         self.db = Database()
-
+        self.db.c.execute("SELECT * FROM main")
+        self.data = self.db.c.fetchall()
+        self.view.setRowCount(len(self.data))
+        for i in range(len(self.data)):
+            for j in range(4):
+                self.view.setItem(i, j, QTableWidgetItem(str(self.data[i][j])))
+        self.show()
 class App(QtWidgets.QMainWindow):
     def __init__(self):
         super(App, self).__init__()
-        self.ui = Ui_MainWindow()
+        self.db = Database()
+        self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.setWindowTitle('codedby:fiverr/linuxkerem')
-        self.ui.addAccount.clicked.connect(self.addAccount)
-        self.ui.addMultipleAccount.clicked.connect(self.addMultipleAccount)
-        self.ui.addAccountGmailTab.clicked.connect(self.addGmailAccount)
-        self.ui.addProxysProxyTab.clicked.connect(self.addProxys)
-        self.ui.addGmailsButton.clicked.connect(self.addGmails)
-        self.ui.addAccountProxyTab.clicked.connect(self.addProxyAccount)
-        self.ui.startButton.clicked.connect(self.start)
+        self.setWindowTitle('codedby:github/linuxkerem')
+        self.ui.dateLabel.setText(str(datetime.datetime.now()).split()[0])
+        self.ui.gelirButton.clicked.connect(self.gelir)
+        self.ui.giderButton.clicked.connect(self.gider)
+        self.ui.clearButton.clicked.connect(self.db.clear)
+        self.ui.showDBButton.clicked.connect(self.showDB)
+    def gelir(self):
+        self.db.insert(self.ui.dateLabel.text(), 1, self.ui.gelirBox.text())
+        self.ui.gelirBox.clear()
+        self.hesapla()
+    def gider(self):
+        self.db.insert(self.ui.dateLabel.text(), 0, self.ui.giderBox.text())
+        self.ui.giderBox.clear()
+        self.hesapla()
+    def hesapla(self):
+        gider = 0
+        gelir = 0
+        giderler = self.db.c.execute("SELECT amount FROM main WHERE type=0 AND date LIKE '%{}%'".format(self.ui.dateLabel.text().split('-')[1]))
+        for i in giderler:
+            gider += i[0]
+        gelirler = self.db.c.execute("SELECT amount FROM main WHERE type=1 AND date LIKE '%{}%'".format(self.ui.dateLabel.text().split('-')[1]))
+        for i in gelirler:
+            gelir += i[0]
+        self.ui.resultLabel.setText("Bu ay ki geliriniz: " + str(int(gelir-gider)))
 
-conn = sqlite3.connect('test.db')
-print("Database opened successfully")
-conn.close()
+    def showDB(self):
+        self.dbWindow = dbWindow()
+        self.dbWindow.show()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec_())
--- requiredLibraries.py
+++ requiredLibraries.py
@@ -3,7 +3,7 @@ import os
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QFileInfo, QSettings
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import qApp, QApplication, QMainWindow, QFormLayout, QLineEdit, QTabWidget, QWidget, QAction, QPushButton, QMessageBox
+from PyQt5.QtWidgets import qApp, QApplication, QMainWindow, QFormLayout, QLineEdit, QTabWidget, QWidget, QAction, QPushButton, QMessageBox, QTableWidget, QHeaderView, QTableWidgetItem
 import itertools
 import time
 import json
